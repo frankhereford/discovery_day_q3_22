0af4362dbab8ff1e38068173f4095d5dae356f69
Frank Hereford <frankhereford@users.noreply.github.com>
2022-08-05 16:08:54 -0500

    muppet sql changes

--- build_layers.sql
+++ build_layers.sql
@@ -23,12 +23,14 @@ create table overlay.muppets (
     
 create table overlay.muppets_history (like overlay.muppets);
 
+
 CREATE TRIGGER versioning_trigger
 BEFORE INSERT OR UPDATE OR DELETE ON overlay.muppets
 FOR EACH ROW EXECUTE PROCEDURE versioning('sys_period',
                                           'overlay.muppets_history',
                                           true);
 
+                                        
 drop view if exists muppets;
 create view muppets as
 select 
@@ -39,12 +41,16 @@ coalesce(overlay.muppets.spooky, foundation.muppets.spooky) as spooky
 from foundation.muppets
 left join overlay.muppets on (overlay.muppets.foundation = foundation.muppets.id);
 
+
+
 insert into foundation.muppets (name, color, spooky) values ('Kermet', 'grey', false) returning id;
 insert into foundation.muppets (name, spooky)        values ('Gonzu', true) returning id;
 insert into foundation.muppets (name, color, spooky) values ('Snarfalopogus', 'blue', true) returning id;
 insert into foundation.muppets (name, color, spooky) values ('Barker', 'yellow', false) returning id;
 insert into foundation.muppets (name, color, spooky) values ('Arminal', 'red', false) returning id;
 insert into foundation.muppets (name, color, spooky) values ('French Chef', 'Fork Fork Fork', false) returning id;
+
+select * from muppets;
 
 insert into overlay.muppets (foundation, name)                values (1, 'Kermit the Frog');
 insert into overlay.muppets (foundation, color, name)         values (2, 'blue', 'Gonzo the Great');
@@ -53,6 +59,8 @@ insert into overlay.muppets (foundation, name, color)         values (4, 'Beaker
 insert into overlay.muppets (foundation, name, spooky)        values (5, 'Animal', true);
 insert into overlay.muppets (foundation, name, color)         values (6, 'Swedish Chef', 'Bork, Bork, Bork!');
 
+select * from muppets;
+
 -- insert into overlay.muppets (foundation, color, name) values (2, 'black','Gonzo the Great');  -- should fail
 -- delete from foundation.muppets where id = 1; -- show cascade delete
 
@@ -60,4 +68,5 @@ select * from overlay.muppets;
 
 update overlay.muppets set color = 'green' where foundation = 1;
 
+select * from overlay.muppets;
 select * from overlay.muppets_history;
